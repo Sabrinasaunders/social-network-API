26ed2df476b6e6a332f585246b078c098666569e
Sabrina Saunders <sabrinamsaunders@gmail.com>
2024-06-02 10:58:33 -0700

    adding code

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-API', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// log the queries being executed
+mongoose.set('debug', true);
+
+// db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+// });
